4f1fc2c0f07b86c80227eb08c04bb4cd3264bdfb
Vinicius Souza <vinicius.souza@rocket-internet.com.br>
2016-03-01 22:02:04 -0300

    Injecting errors module

--- src/app/sections/sections.module.js
+++ src/app/sections/sections.module.js
@@ -2,12 +2,14 @@ import main from './main/main.module';
 import authentication from './authentication/authentication.module';
 import map from './map/map.module';
 import profile from './profile/profile.module';
+import errors from './errors/errors.module';
 import route from './sections.route';
 
 export default angular.module('mosquito.sections', [
     main.name,
     authentication.name,
     map.name,
+    errors.name,
     profile.name
   ])
   .config(route);
