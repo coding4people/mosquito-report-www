66108eb55d568a50024325bb3995145a156e6e86
Vinicius Souza <vinicius.souza@rocket-internet.com.br>
2016-03-01 22:01:40 -0300

    Adding authentication rule for section state

--- src/app/sections/sections.route.js
+++ src/app/sections/sections.route.js
@@ -1,9 +1,18 @@
 export default function routerConfig ($stateProvider) {
   'ngInject';
 
+  var UserNotLoggedIn = function (user, params, location) {
+    if (!user.auth.token) {
+      return {to: 'authentication.signIn', params: {redirect: decodeURIComponent(location)}};
+    }
+  };
+
   $stateProvider
     .state('sections', {
       abstract: true,
-      templateUrl: 'app/sections/sections.html'
+      templateUrl: 'app/sections/sections.html',
+      'data': {
+        rule: UserNotLoggedIn
+      }
     });
 }
