ea4ee6652da7adca59097ff12065ce1177ba668f
Vinicius Souza <vinicius.souza@rocket-internet.com.br>
2016-03-01 22:03:28 -0300

    Declarating SignInService

--- src/app/sections/authentication/signIn/signIn.module.js
+++ src/app/sections/authentication/signIn/signIn.module.js
@@ -1,6 +1,8 @@
 import controller from './signIn.controller';
+import service from './signIn.service';
 import routeConfig from './signIn.route';
 
 export default angular.module('mosquito.sections.authentication.signIn', [])
   .config(routeConfig)
+  .service('SignInService', service)
   .controller('SignInController', controller);
