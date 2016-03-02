b0ad31c96e49c4086da1b81c5582f771c7a7f8ea
Vinicius Souza <vinicius.souza@rocket-internet.com.br>
2016-03-01 22:00:50 -0300

    wip signIn controller

--- src/app/sections/authentication/signIn/signIn.controller.js
+++ src/app/sections/authentication/signIn/signIn.controller.js
@@ -1,9 +1,36 @@
 export default class SignInController {
-  constructor() {
+  constructor($scope, $rootScope, SignInService, toastr, $stateParams, $location, $state, Messages) {
     'ngInject';
+
+    this.fields = {
+      credentials: {
+        email: null,
+        rawPassword: null
+      }
+    };
+    this.$scope = $scope;
+    this.SignInService = SignInService;
+    this.toastr = toastr;
+    this.isLoading = false;
+    this.redirectTo = $stateParams.redirect;
+    this.$location = $location;
+    this.$state = $state;
+    this.$rootScope = $rootScope;
+    this.Messages = Messages;
   }
 
-  x() {
-    return true;
+  submit(isValid) {
+    if(isValid) {
+      this.SignInService.submitLogin(this.fields);
+    }
+  }
+
+  redirect() {
+    if(this.redirectTo){
+      this.$location.path(decodeURIComponent(this.redirectTo)).search({});
+      this.$location.replace();
+    }else{
+      this.$state.go('sections.map');
+    }
   }
 }
