dc79c0c3ce6ebfa40a16c00d56bfae4bd67aba8c
Vinicius Souza <vinicius.souza@rocket-internet.com.br>
2016-03-03 23:13:16 -0300

    Injecting formUtilities

--- src/app/components/components.module.js
+++ src/app/components/components.module.js
@@ -1,7 +1,9 @@
 import mapHelper from './mapHelper/mapHelper.module';
 import localStorage from './localStorage/localStorage.module';
+import formUtilities from './formUtilities/formUtilities.module';
 
 export default angular.module('mosquito.components', [
   mapHelper.name,
-  localStorage.name
+  localStorage.name,
+  formUtilities.name
 ]);
