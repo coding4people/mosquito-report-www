8a23b6f973093b74d0ab4101b052a81a3a0d4c6d
Vinicius Souza <vinicius.souza@rocket-internet.com.br>
2016-03-06 19:36:29 -0300

    Declaring section-header into signUp.route

--- src/app/sections/authentication/signup/signUp.route.js
+++ src/app/sections/authentication/signup/signUp.route.js
@@ -7,6 +7,9 @@ export default function routerConfig ($stateProvider, CurrentLocaleProvider) {
     .state('authentication.signUp', {
       url: '/sign-up',
       views: {
+        'section-header': {
+          templateUrl: `app/sections/authentication/signUp/signUpHeader.${lang}.html`
+        },
         'authentication-section': {
           templateUrl: `app/sections/authentication/signUp/signUp.${lang}.html`,
           controller: 'SignUpController',
